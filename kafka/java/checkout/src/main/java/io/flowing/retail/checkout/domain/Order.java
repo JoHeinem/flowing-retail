da338a6075582f4165299661d1a2570afc2e549a
Johannes Heinemann <johannes.heinemann@camunda.com>
2019-06-11 15:49:35 +0200

    make new shop page work

--- kafka/java/checkout/src/main/java/io/flowing/retail/checkout/domain/Order.java
+++ kafka/java/checkout/src/main/java/io/flowing/retail/checkout/domain/Order.java
@@ -1,9 +1,12 @@
 package io.flowing.retail.checkout.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Order {
   
   private String orderId = "checkout-generated-" + UUID.randomUUID().toString();
--- kafka/java/checkout/src/main/java/io/flowing/retail/checkout/rest/ShopRestController.java
+++ kafka/java/checkout/src/main/java/io/flowing/retail/checkout/rest/ShopRestController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +45,7 @@ public class ShopRestController {
   }
 
   @RequestMapping(path = "/api/order", method = PUT, consumes = {MediaType.APPLICATION_JSON_VALUE})
-  public String placeOrder(Order order) {
+  public String placeOrder(@RequestBody Order order) {
 
     logger.info("The following order has been received: {}", order.toString());
 
